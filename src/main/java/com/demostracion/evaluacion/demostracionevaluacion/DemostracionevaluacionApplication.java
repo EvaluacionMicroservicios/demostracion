c1122804b16a781f1d1e12afa2675ae6a06b842b
Ronnie Gonzales <ronnie_cepafs@hotmail.com>
2025-01-25 05:46:27 -0500

    Primer commit

---
+++ src/main/java/com/demostracion/evaluacion/demostracionevaluacion/DemostracionevaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.demostracion.evaluacion.demostracionevaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemostracionevaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemostracionevaluacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demostracion/evaluacion/demostracionevaluacion/DemostracionevaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demostracion.evaluacion.demostracionevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemostracionevaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
